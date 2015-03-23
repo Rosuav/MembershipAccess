e497264dabdefde92e012e6ea8c195d7b6f3a971
Chris Angelico <rosuav@gmail.com>
2015-03-23 18:27:53 +1100

    Allow HTML generation

--- membaccess.py
+++ membaccess.py
@@ -1,6 +1,9 @@
 import os
 import csv
 import sys
+import time
+
+tempfn = "/tmp/membership.csv"
 
 def auth():
 	from pydrive.auth import GoogleAuth
@@ -29,7 +32,34 @@ def cmd_fetch():
 		file.metadata['exportLinks'][u'text/csv'] = file.metadata['exportLinks'][u'application/pdf'][:-3]+u"csv"
 	# Current versions of PyDrive don't support GetContentString() with a mimetype parameter, but the Python
 	# 'csv' module doesn't support reading from a string anyway, so we dump out to a file.
-	file.GetContentFile("membership.csv",mimetype="text/csv")
+	file.GetContentFile(tempfn,mimetype="text/csv")
+
+def get_file():
+	# Freshen the file if it's more than an hour old, or doesn't exist.
+	try:
+		if time.time()-os.stat(tempfn).st_mtime > 3600: cmd_fetch()
+	except OSError:
+		cmd_fetch()
+	# It should now be possible to read tempfn. (Should. It could still raise.)
+
+def cmd_columns():
+	get_file()
+	with open(tempfn) as f:
+		print(next(csv.reader(f)))
+
+def cmd_html():
+	get_file()
+	cols = ['Title', 'First Name', 'Surname', 'SortCode']
+	print("<!doctype html>")
+	print("<html>")
+	print("<head><title>G&S Society Membership</title></head>")
+	print("<body><table border>")
+	print("<tr><td>"+"</td><td>".join(cols)+"</td></tr>")
+	with open(tempfn) as f:
+		for row in csv.DictReader(f):
+			print("<tr><td>"+"</td><td>".join(row[col] for col in cols)+"</td></tr>")
+	print("</table></body>")
+	print("</html>")
 
 # If error, throw exception. Bahahaha.
 globals()["cmd_"+sys.argv[1]]()
